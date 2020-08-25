f8e1a0df8501a86e194f7452d9f3568077f931fb
Richard Mah <dick.mah@gmail.com>
2020-08-25 14:40:28 -0400

    Add default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -6,6 +6,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+variable "public_key_path" {}
+variable "private_key_path" {}
+
+resource "random_pet" "this" {}
+
 module "defaults" {
   source = "../.."
+  name   = random_pet.this.id
+
+  private_key_path = var.private_key_path
+  public_key_path  = var.public_key_path
 }
--- test/terraform_defaults_test.go
+++ test/terraform_defaults_test.go
@@ -3,12 +3,17 @@ package test
 import (
 	"fmt"
 	"github.com/gruntwork-io/terratest/modules/aws"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"io/ioutil"
 	"path"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestTerraformDefaults(t *testing.T) {
@@ -30,6 +35,12 @@ func TestTerraformDefaults(t *testing.T) {
 		test_structure.SaveEc2KeyPair(t, exampleFolder, keyPair)
 
 		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, exampleFolder)
+
+		testEndpoint(t, terraformOptions)
 	})
 }
 
@@ -60,11 +71,37 @@ func configureTerraformOptions(t *testing.T, exampleFolder string) (*terraform.O
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
 			"aws_region":    awsRegion,
-			//"public_key_path":    publicKeyPath,
-			//"private_key_path": privateKeyPath,
+			"public_key_path":    publicKeyPath,
+			"private_key_path": privateKeyPath,
 		},
 	}
 
 	return terraformOptions, keyPair
 }
 
+func testEndpoint(t *testing.T, terraformOptions *terraform.Options) {
+
+	endpointIP := terraform.Output(t, terraformOptions, "dns_name")
+
+	expectedStatus := "200"
+	url := fmt.Sprintf("http://%s:3000", endpointIP)
+
+	description := fmt.Sprintf("curl to LB %s with error command", endpointIP)
+	maxRetries := 60
+	timeBetweenRetries := 2 * time.Second
+
+	retry.DoWithRetry(t, description, maxRetries, timeBetweenRetries, func() (string, error) {
+
+		outputStatus, _, err := http_helper.HTTPDoE(t, "GET", url, nil, nil,nil)
+
+		if err != nil {
+			return "", err
+		}
+
+		if strings.TrimSpace(strconv.Itoa(outputStatus)) != expectedStatus {
+			return "", fmt.Errorf("expected '%s' but got '%s'", expectedStatus, strconv.Itoa(outputStatus))
+		}
+
+		return "", nil
+	})
+}
